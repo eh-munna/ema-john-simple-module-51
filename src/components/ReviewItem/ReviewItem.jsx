040ce88d5da52e8f2c366b78da0324dfc45532f2
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-04-08 18:33:26 +0200

    55-5 More style of Review item and font awesome delete style

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { TrashIcon } from '@heroicons/react/24/solid';
+
 import './ReviewItem.css';
 const ReviewItem = ({ product }) => {
-  const { img, price, name, shipping } = product;
+  const { img, price, name, shipping, quantity } = product;
   console.log(product);
   return (
     <div className="review-item">
-      <div>
+      <div className="review-img">
         <img src={img} alt="" />
       </div>
-      <div>
+      <div className="review-info">
         <h4>{name}</h4>
+        <h5>
+          Quantity : <span>{quantity}</span>
+        </h5>
         <h5>
           Price : <span>${price}</span>
         </h5>
@@ -17,8 +22,10 @@ const ReviewItem = ({ product }) => {
           Shipping Charge : <span>${shipping}</span>
         </h5>
       </div>
-      <div>
-        <h5>D</h5>
+      <div className="review-del">
+        <button className="">
+          <TrashIcon className="flex justify-center items-center w-6 h-6 text-blue-500" />
+        </button>
       </div>
     </div>
   );
